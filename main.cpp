7398be7aa0b091a2a9889fbcb62368855ee85a42
Imani Dap <imani.dap@gmail.com>
2019-04-22 10:54:55 +0200

    chore(heart): replace servo with motor disc

--- main.cpp
+++ main.cpp
@@ -1,24 +1,31 @@
 #include <Servo.h>
 
-Servo heart;
+int heartPin = 13; 
 Servo tail;
 
 int tailPosition = 0;
 void setup()
 {
-    heart.attach(13);
-    tail.attach(12);
+    pinMode(heartPin, OUTPUT);
 
-    heart.write(0);
+    tail.attach(12);
     tail.write(0);
 }
 
 void loop()
 {
-    heart.write(30);
-    delay(600); 
-    heart.write(0);
-    delay(1200); 
+
+    //beat 1
+    digitalWrite(heartPin, HIGH);
+    delay(80); 
+    digitalWrite(heartPin, LOW);
+    delay(500); 
+
+    //beat 2
+    digitalWrite(heartPin, HIGH);
+    delay(50); 
+    digitalWrite(heartPin, LOW);
+    delay(800);
 
     if(tailPosition != 90) {
         tailPosition = 90;
